3067884184097d6a125b7598f99e975055d6b21e
jtsay <jtsay@cloudreactor.io>
2021-11-27 22:32:46 -0800

    Release 1.3.0

--- build_release.sh
+++ build_release.sh
@@ -10,9 +10,9 @@ docker build -t aws-setup-wizard -t \
 docker login
 docker tag aws-setup-wizard cloudreactor/aws-setup-wizard:latest
 docker push cloudreactor/aws-setup-wizard:latest
-docker tag aws-setup-wizard cloudreactor/aws-setup-wizard:1.2.1
-docker push cloudreactor/aws-setup-wizard:1.2.1
-docker tag aws-setup-wizard cloudreactor/aws-setup-wizard:1.2
-docker push cloudreactor/aws-setup-wizard:1.2
+docker tag aws-setup-wizard cloudreactor/aws-setup-wizard:1.3.0
+docker push cloudreactor/aws-setup-wizard:1.3.0
+docker tag aws-setup-wizard cloudreactor/aws-setup-wizard:1.3
+docker push cloudreactor/aws-setup-wizard:1.3
 docker tag aws-setup-wizard cloudreactor/aws-setup-wizard:1
 docker push cloudreactor/aws-setup-wizard:1
